5962baeaa32ed00394475f6b8e095db0a852c261
Nikita <nrusanovskii923@gmail.com>
2025-04-04 15:08:28 +0300

    Перевёл на английский комментарии, корректно перевел "пульт"

--- my_implementation_of_the_command_pattern/main.cpp
+++ my_implementation_of_the_command_pattern/main.cpp
@@ -3,117 +3,115 @@
 using namespace std;
 
 /* ----------------------------------------------------------------------------------------
-	
-	Суть паттерна:
-	
-	Инкапсулируем запрос на выполнение какого-то действия в отдельный объект (команду).
-	
-	При этом объекты инициирующие запросы и объекты выполняющие их отделяются друг от
-	друга.
 
-	В данном паттерне участвуют:
+    Essence of the pattern:
 
-	Command - интерфейс команды.
-	ConcreteCommand - конкретная команда, которая выполняет n-ное действие.
-	Receiver - интерфейс получателя, определяет действия, которые будет выполнять команда.
-	Invoker - инициатор команды, он будет вызывать команду для выполнения запроса.
-	Client - создает команду и устанавливает её получателя.
+    We encapsulate a request to perform some action in a separate object (command).
+
+    At the same time, the objects initiating requests and the objects executing them
+    are separated from each other.
+
+    The following participants are involved in this pattern:
+
+    Command - command interface.
+    ConcreteCommand - a specific command that performs a certain action.
+    Receiver - the recipient interface, defines the actions that the command will perform.
+    Invoker - the command initiator, it will call the command to execute the request.
+    Client - creates the command and assigns its receiver.
 
 ---------------------------------------------------------------------------------------- */
 
-class Receiver { // интерфейс получателя, он определит действия команд
+class Receiver { // receiver interface, defines command actions
 public:
-	Receiver() = default;
-	virtual void commandOn() = 0;
-	virtual void commandOff() = 0;
+    Receiver() = default;
+    virtual void commandOn() = 0;
+    virtual void commandOff() = 0;
 };
 
-class Command { // интерфейс команды
+class Command { // command interface
 protected:
-	Receiver* rec;
+    Receiver* rec;
 public:
-	Command(Receiver* rec = nullptr) : rec(rec) {}
-	virtual ~Command() = default;
-	virtual void execute() = 0;
+    Command(Receiver* rec = nullptr) : rec(rec) {}
+    virtual ~Command() = default;
+    virtual void execute() = 0;
 };
 
-class Invoker { // инициатор команд
+class Invoker { // command initiator
 protected:
-	Command* com = nullptr;
+    Command* com = nullptr;
 public:
-	Invoker() = default;
-	virtual ~Invoker() { delete com; }
-	
-	void executeCommand() {
-		if (com != nullptr) {
-			com->execute();
-		}
-	}
+    Invoker() = default;
+    virtual ~Invoker() { delete com; }
 
-	void setCommand(Command* newCom) {
-		if (com != newCom) {
-			if (com != nullptr) {
-				delete com;
-			}
-			com = newCom;
-		}
-	}
+    void executeCommand() {
+        if (com != nullptr) {
+            com->execute();
+        }
+    }
 
+    void setCommand(Command* newCom) {
+        if (com != newCom) {
+            if (com != nullptr) {
+                delete com;
+            }
+            com = newCom;
+        }
+    }
 };
 
 /*
 
-	После подготовки необходимых интерфейсов и написания инициатора команд,
-	используем их для реализации паттерна на примере системы классов:
-	Телевизор <- Пульт <- Пользователь
+    After preparing the necessary interfaces and writing the command initiator,
+    we use them to implement the pattern using a class system example:
+    TV <- Remote <- User
 
 */
 
 class ButtonOn : public Command {
 public:
-	ButtonOn(Receiver* rec) : Command(rec) {}
-	void execute() override {
-		rec->commandOn();
-	}
+    ButtonOn(Receiver* rec) : Command(rec) {}
+    void execute() override {
+        rec->commandOn();
+    }
 };
 
 class ButtonOff : public Command {
 public:
-	ButtonOff(Receiver* rec) : Command(rec) {}
-	void execute() override {
-		rec->commandOff();
-	}
+    ButtonOff(Receiver* rec) : Command(rec) {}
+    void execute() override {
+        rec->commandOff();
+    }
 };
 
-class Pult : public Receiver {
+class Remote : public Receiver {
 public:
-	Pult() = default;
-	void commandOn() override {
-		cout << "TV is on" << endl;
-	}
-	void commandOff() override {
-		cout << "TV is off" << endl;
-	}
+    Remote() = default;
+    void commandOn() override {
+        cout << "TV is on" << endl;
+    }
+    void commandOff() override {
+        cout << "TV is off" << endl;
+    }
 };
 
 class User : public Invoker {
 public:
-	User() = default;
+    User() = default;
 };
 
 int main() {
 
-	User user;
-	Pult* pult = new Pult;
-	ButtonOn* bon = new ButtonOn(pult);
-	ButtonOff* boff = new ButtonOff(pult);
+    User user;
+    Remote* remote = new Remote;
+    ButtonOn* bon = new ButtonOn(remote);
+    ButtonOff* boff = new ButtonOff(remote);
 
-	user.setCommand(bon);
-	user.executeCommand();
+    user.setCommand(bon);
+    user.executeCommand();
 
-	user.setCommand(boff);
-	user.executeCommand();
+    user.setCommand(boff);
+    user.executeCommand();
 
-
-	return 0;
+    return 0;
 }
